b53f02d0215680a1de54ed1381577a3024aa4d21
AlexisKueny <alexis.kueny@outlook.com>
2025-03-11 16:28:30 +0100

    Created new function prototypes

--- puzzle_solver.py
+++ puzzle_solver.py
@@ -3,18 +3,32 @@
 from PIL import Image
 import cv2
 
+class PuzzlePiece(self,image):
+    # This class stores all necesary details about each piece
+    # Store image, corners, contours, etc.
+    # Additional methods to rotate and set position
+
 def process_image(path:str):
     img = cv2.imread(path)
     # apply grayscale and binarization on picture
     # copy modified picture into new directory
 
 def detect_pieces(path:str):
-    # Extract jigsaw pieces from processed image
-    # Save all pieces into a new directory
+    # Identify puzzle pieces on image using edge detection
+    # Extract and highlight contours
 
-def identify_pieces(path:str):
-    # For each piece, create a new puzzle piece object
-    # Each puzzle piece 
+def segment_pieces(path:str):
+    # Segment the image into individual puzzle piece
+    # Crop image using the contours
+    # Make sure each piece is isolated
+
+def extract_features(piece_path:str):
+    # Extract key features from piece such as edges, corner feature and texture patterns
+    # Create a puzzle piece object for each piece
+
+def assemble_puzzle():
+    # Assemble final picture using puzzle piece edges and corners.
+    # Can be done using BFS / DFS
 
 def solve_puzzle(image_path):
     print("Doing some work")
