8166c34aa4d247956f67aa0df248d5ecbc950aa5
AlexisKueny <alexis.kueny@outlook.com>
2025-03-12 12:43:21 +0100

    Camera calibration module

---
+++ camera_calibration.py
@@ -0,0 +1,75 @@
+"""import cv2
+import numpy as np
+import glob
+
+CHECKERBOARD = (6,9)
+images = glob.glob('images/calibration/*.jpg')
+print(images)
+
+for fname in images:
+    img = cv2.imread(fname)
+    img = cv2.resize(img,(int(400),int(400)))
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+    print(corners)
+
+    corners = np.int_(corners)
+
+    for i in corners:
+        x,y = i.ravel()
+        cv2.circle(img,(x,y),3,(0,0,255),-1)
+
+    cv2.imshow('Corners',img)
+
+    cv2.waitKey(0)"""
+
+"""
+--------------------------------------------------  Calibration -----------------------------------------------------------------
+"""
+
+import numpy as np
+import cv2 as cv,cv2
+import glob
+
+# Insert number of rows and columns of checkerboard model in CHECKERBOARD
+CHECKERBOARD = (6,9)
+ 
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+ 
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:6,0:9].T.reshape(-1,2)
+ 
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+ 
+images = glob.glob('images/calibration/*.jpg')
+print(images)
+ 
+for fname in images:
+    img = cv.imread(fname)
+    img = cv2.resize(img,(400,400))
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ 
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray,CHECKERBOARD,None)
+ 
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+ 
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(0)
+ 
+cv.destroyAllWindows()
+
+# Get camera matrix, distortion coefficients, rotation and translation vectors
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
--- puzzle_solver.py
+++ puzzle_solver.py
@@ -1,34 +1,43 @@
 # puzzle_solver.py
 
 from PIL import Image
-import cv2
+import cv2 as cv,cv2
+import numpy as np
+import glob
 
-class PuzzlePiece(self,image):
+#class PuzzlePiece(self,image):
     # This class stores all necesary details about each piece
     # Store image, corners, contours, etc.
     # Additional methods to rotate and set position
 
 def process_image(path:str):
     img = cv2.imread(path)
-    # apply grayscale and binarization on picture
-    # copy modified picture into new directory
+
+    # Perform camera calibration
+
+    return
+
 
 def detect_pieces(path:str):
     # Identify puzzle pieces on image using edge detection
     # Extract and highlight contours
+    return
 
 def segment_pieces(path:str):
     # Segment the image into individual puzzle piece
     # Crop image using the contours
     # Make sure each piece is isolated
+    return
 
 def extract_features(piece_path:str):
     # Extract key features from piece such as edges, corner feature and texture patterns
     # Create a puzzle piece object for each piece
+    return
 
 def assemble_puzzle():
     # Assemble final picture using puzzle piece edges and corners.
     # Can be done using BFS / DFS
+    return
 
 def solve_puzzle(image_path):
     print("Doing some work")
@@ -47,3 +56,6 @@ def solve_puzzle(image_path):
 
     return solution
 
+if __name__ == '__main__':
+    print("hello")
+    
